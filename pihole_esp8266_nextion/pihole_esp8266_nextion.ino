3cecfe3214ce757b7defe93a8aadfb44e787f8e0
GameWatch <78940573+GameWatchProjects@users.noreply.github.com>
2021-02-15 07:08:36 +0100

    Update
    Add more statistic's to display

--- pihole_esp8266_nextion/pihole_esp8266_nextion.ino
+++ pihole_esp8266_nextion/pihole_esp8266_nextion.ino
@@ -30,8 +30,9 @@ WiFiClient client; // create wifi client object
 
 const char* host         = "192.168.XXX.XXX";           // Pi-Hole IP - NOTICE: Make sure, the Pi-Hole and NTP service installation is on the same maschine
 const char* ssid         = "YOUR-WIFI-SSID";            // Wi-Fi SSID Name
-const char* password     = "YOUR-SECRET-WIFI-PASSWORD"; // Wi-Fi Password
+const char* password     = "YOUR-WIFI-PASS";            // Wi-Fi Password
 const char* wifihost     = "pihole-display";            // Wi-Fi Hostname
+
 
 // You need to adjust the UTC offset for your timezone in milliseconds
 //    For UTC -5.00 : -5 * 60 * 60 = -18000
@@ -44,6 +45,124 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, host, utcOffsetInSeconds); 
 
 
+void setup() {
+    // Deault Baudrate by the Nextion displays is 9600, but temporary the default baud rate 
+    // will change temporary to 115200 by using the given nextion tft file
+    Serial.begin(115200);
+    delay(1000);
+    Start_WiFi(ssid,password);
+    timeClient.begin();
+}
+
+
+void loop() {
+       
+   if((WiFi.status() == WL_CONNECTED)) {
+        timeClient.update();
+        HTTPClient http;
+        http.begin("http://"+ String(host) +"/admin/api.php?summary"); 
+        int httpCode = http.GET();
+        if(httpCode > 0) {
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();       // save as string 'payload'
+                const size_t bufferSize = JSON_OBJECT_SIZE(10) + 250;
+                DynamicJsonBuffer jsonBuffer(bufferSize);
+                JsonObject& root = jsonBuffer.parseObject(payload);
+                JsonObject& response = root["response"];
+                JsonObject& response_data0 = response["data"][0];
+                const char* domains_being_blocked = root["domains_being_blocked"]; 
+                const char* dns_queries_today = root["dns_queries_today"]; 
+                const char* ads_blocked_today = root["ads_blocked_today"]; 
+                const char* ads_percentage_today = root["ads_percentage_today"]; 
+                const char* unique_domains = root["unique_domains"]; 
+                const char* queries_forwarded = root["queries_forwarded"]; 
+                const char* queries_cached = root["queries_cached"]; 
+                const char* clients_ever_seen = root["clients_ever_seen"]; 
+                const char* unique_clients = root["unique_clients"]; 
+                const char* status_pihole = root["status"];
+                const char* update_blocklist_days = root["gravity_last_updated"]["relative"]["days"];
+                const char* update_blocklist_hours = root["gravity_last_updated"]["relative"]["hours"];
+                const char* update_blocklist_minutes = root["gravity_last_updated"]["relative"]["minutes"];
+
+                         
+                String command1 = "start_page.ads.txt=\"" + String(dns_queries_today) + "\"";
+                Serial.print(command1);
+                endNextionCommand();
+                delay(10);
+                String command2 = "start_page.clients.txt=\"(Clients: unique: " + String(unique_clients) + " / ever seen: " + String(clients_ever_seen) + ")\"";
+                Serial.print(command2);
+                endNextionCommand();
+                delay(10);
+                String command3 = "start_page.domains.txt=\"" + String(domains_being_blocked) + "\"";
+                Serial.print(command3);
+                endNextionCommand();
+                delay(10);
+                String command4 = "start_page.today.txt=\"" + String(ads_percentage_today) + "%\"";
+                Serial.print(command4);
+                endNextionCommand();
+                delay(10);
+                String command5 = "start_page.blocked.txt=\"" + String(ads_blocked_today) + "\"";
+                Serial.print(command5);
+                endNextionCommand();
+                delay(10);
+                String command6 = "start_page.update.txt=\"(Last update: " + String(update_blocklist_days) + " days / " + String(update_blocklist_hours) + " hours / " + String(update_blocklist_minutes) + " minutes)\"";
+                Serial.print(command6);
+                endNextionCommand();
+                delay(10);
+                                
+                if (String(status_pihole) == "enabled") {
+                   String command7 = "start_page.status.pic=2";
+                   Serial.print(command7);
+                   endNextionCommand();
+                   delay(10);
+                   String command8 = "start_page.status.txt=\"" + String(timeClient.getFormattedTime()) + "\"";
+                   Serial.print(command8);
+                   endNextionCommand();
+                   delay(10);
+ 
+                }
+                else {
+                   String command9 = "start_page.status.pic=1";
+                   Serial.print(command9);
+                   endNextionCommand();
+                   delay(10);
+                   String command10 = "start_page.status.txt=\"" + String(timeClient.getFormattedTime()) + "\"";
+                   Serial.print(command10);
+                   endNextionCommand();
+                   delay(10);
+
+                }
+                
+                http.end();
+            }
+        } else {
+                   String command11 = "start_page.status.txt=\"No WiFi\"";
+                   Serial.print(command11);
+                   endNextionCommand();
+       }
+    }
+        delay(10000); // 10 sec. refresh delay
+}
+
+
+void endNextionCommand(){
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+}
+
+
+int Start_WiFi(const char* ssid, const char* password){
+    int connAttempts = 0;
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED ) {
+      WiFi.hostname(wifihost);
+        delay(500);
+        if(connAttempts > 20) return -5;
+        connAttempts++;
+        }
+    return 1;
+}
 void setup() {
     Serial.begin(9600);
     delay(1000);
